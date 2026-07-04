bfd8d6e02d1e1430001fa52249c8418c40dc406a
Кайнар Толеубаев <kainar@MacBook-Pro.local>
2026-07-05 01:22:23 +0500

    feat: add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,21 @@
+/// <reference types="node" />
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   css: ['~/assets/css/main.css'],
   app: {
     baseURL: '/window-resizer-wp/',
     head: {
+      script:
+        process.env.NODE_ENV === 'production'
+          ? [
+              {
+                defer: true,
+                src: 'https://cloud.umami.is/script.js',
+                'data-website-id': '977297c5-5821-42b5-bdab-0336dc9c71ae',
+              },
+            ]
+          : [],
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.png' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
