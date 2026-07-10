162256dc37f8b5bc3c9d2179ea48b12f2239652f
Кайнар Толеубаев <kainar@MacBook-Pro.local>
2026-07-10 19:15:35 +0500

    feat: change extension url

--- constants/extension.ts
+++ constants/extension.ts
@@ -1,1 +1,1 @@
-export const CHROME_EXTENSION_URL = 'https://chromewebstore.google.com/'
+export const CHROME_EXTENSION_URL = 'https://chromewebstore.google.com/detail/window-resizer/cpddaepajglabiefkhnofjmjacdclkbc?hl=en'
